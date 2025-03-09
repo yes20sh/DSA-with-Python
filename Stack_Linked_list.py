f8c21353740d8cd97c4c89fdfe268240fdb30d9a
Yashraj Singh Solanki <yes20sh@gmail.com>
2025-03-09 14:12:21 +0000

    feat: Implement additional Stack methods and Text Editor functionality

--- Stack_Linked_list.py
+++ Stack_Linked_list.py
@@ -19,6 +19,13 @@ class StackLinkedList:
             return "Empty"
         else:
             return "Not Empty"
+    
+    def clear(self):
+        self.top = None
+    
+    def sizeof(self):
+        return self.size
+   
         
     def peak(self):
         if self.epty():
@@ -48,9 +55,57 @@ class StackLinkedList:
         curr = self.top
         result = " "
         while curr != None:
-            result = result + str(curr.data) + " -> "
+            result = str(curr.data) + " " +  result 
             curr = curr.next
-        return result[:-3]
+        return result
+
+    # -------------------------------------------------------------------------------------
+
+    # Q - 1 reverse the string
+    def reverse(self):
+        temp_stack = StackLinkedList()
+        result = " "
+        while not self.epty():
+            item = self.pop()
+            temp_stack.push(item)
+            result =   result + str(item) + " "
+        self.top = temp_stack.top
+        return result
+            
+            
+
+    # Q - 2 Text editor
+
+    def text_editor(self):
+        redo = StackLinkedList()
+        while True:
+            option = input('''
+                Welcome to Text editor!
+                - Enter 1 for Undo
+                - Enter 2 for Redo
+                - Enter 3 for Exit
+                Choose your option : ''')
+            if option == '1':
+                if not self.epty():
+                    item = self.pop()
+                    redo.push(item)
+                    print(f"Undo {item} & current stack {self}")
+                else:
+                    print("Empty stack")
+                
+            elif option == '2':
+                if redo.pop() is not None:
+                    item = redo.pop()
+                    self.push(item)
+                    print(f"Undo {item} & current stack {self}")
+                else:
+                    print("No redo available")
+
+            elif option == '3':
+                break
+            else : 
+                print("Wrong Option")
+0        
 
 stk = StackLinkedList()
 
@@ -62,6 +117,21 @@ print(f"Stack is empty or not :  {stk.empty()} ")
 print(f"The peak element in array : {stk.peak()}")
 print(stk)
 stk.pop()
+print(f"Size of stack : {stk.sizeof()}")
+print(stk)
+stk.clear()
+
+# q1
+stk.push("O")
+stk.push("L")
+stk.push("L")
+stk.push("E")
+stk.push("H")
+print(stk)
+print(f" Reverse the stack string : {stk.reverse()}")
+
+# Q2
+stk.text_editor()
 print(stk)
 
 
