e16adb7fe4847273a8f665d5b7229077fca6a879
Yashraj Singh Solanki <yes20sh@gmail.com>
2025-04-07 18:46:29 +0000

    feat: implement function to remove extra spaces from a string

--- String/pb-5.py
+++ String/pb-5.py
@@ -1,1 +1,10 @@
 # Remove leading, trailing, and extra spaces in a string.
+
+def clean_spaces(s):
+    words = s.strip().split()
+    cleaned_string = ' '.join(words)
+    return cleaned_string
+
+input_str = "   Hello     world!   This   is   Python.   "
+result = clean_spaces(input_str)
+print("Cleaned String:", result)
